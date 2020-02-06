eebc531ca9a51ea6e5f50cf31a72fe81640e8a9b
Haoyuan Li <hylli@ucdavis.com>
2020-02-06 15:16:30 -0800

    change path to ASPECT_PROJECT folder

--- aspect_enable.sh
+++ aspect_enable.sh
@@ -3,7 +3,5 @@ export ASPECT_SOURCE_DIR="$HOME/softwares/aspect"
 
 export Aspect_DIR="$HOME/softwares/aspect/build"
 
-export PATH="$PATH:$HOME/Desktop/ASPECT_PROJECT/files"
-
 export PATH="$PATH:$HOME/ASPECT_PROJECT/files"
 
