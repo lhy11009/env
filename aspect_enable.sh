45aa9f5e854aa2a14630b52fdc9ad89d0829bae9
Haoyuan Li <lochy@peloton.cse.ucdavis.edu>
2020-05-27 16:23:53 -0700

    Add project_TwoDSubduction.sh

--- aspect_enable.sh
+++ aspect_enable.sh
@@ -1,9 +1,9 @@
 
-export ASPECT_SOURCE_DIR="$HOME/softwares/aspect"
+export ASPECT_SOURCE_DIR="$HOME/software/aspect"
 
-export Aspect_DIR="$HOME/softwares/aspect/build"
+export Aspect_DIR="$HOME/software/aspect/build"
 
 export PATH="$PATH:$HOME/Desktop/ASPECT_PROJECT/files"
 
-export PATH="$PATH:$HOME/ASPECT_PROJECT/files"
+export PATH="$PATH:$HOME/ASPECT_PROJECT/aspectLib"
 
